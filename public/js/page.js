d039e59a87c72afa25a22bc2f8e125065b669a96
niC00L <nika.blanarova@gmail.com>
2016-05-18 21:39:21 +0200

    Highscore

--- public/js/page.js
+++ public/js/page.js
@@ -9,19 +9,19 @@ var openButton = document.getElementsByClassName('open');
 var closeButton = document.getElementsByClassName('close');
 var colorsHelp = document.querySelector('#colors');
 
-for (var i = 0; i < openButton.length; i++) {		
+for (var i = 0; i < openButton.length; i++) {
 	openButton[i].addEventListener('click', function (e) {
-		var open = this.dataset.open;	
+		var open = this.dataset.open;
 		e.preventDefault();
-		document.getElementById(open).className = 'visible';
+		document.getElementById(open).classList.toggle('hidden');
 	}, false);
 }
 
-for (var i = 0; i < closeButton.length; i++) {		
+for (var i = 0; i < closeButton.length; i++) {
 	closeButton[i].addEventListener('click', function (e) {
 		var close = this.dataset.close;
 		e.preventDefault();
-		document.getElementById(close).className = 'hidden';
+		document.getElementById(close).classList.toggle('hidden');
 	}, false);
 }
 
@@ -42,4 +42,27 @@ function drawColors() {
 }
 
 drawColors();
+
+var oReq = new XMLHttpRequest();
+oReq.onload = function (e) {
+	var response = JSON.parse(oReq.response);
+	printScore(response);
+}
+oReq.open("GET", "http://miby.ienze.me/api/top");
+oReq.send();
+
+function printScore(response) {
+	var table = document.querySelector('#scoreTable tbody');
+	var v;
+	var elem;
+	for (var key in response) {
+		if (key < 10) {
+			v = response[key];
+			r = parseInt(key) + 1;
+			elem = '<tr><td>' + r + '</td><td>' + v['name'] + '</td><td>' + v['level'] + '</td></tr>'
+			table.insertAdjacentHTML('beforeend', elem);
+		}
+	}
+	console.log(table.innerHtml);
+}
 
