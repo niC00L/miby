d27c62c91622ec1c00458d4bc4247c9fe8efe714
Linzee <d.gmiterko@gmail.com>
2016-05-18 22:02:19 +0200

    Cors fix?

--- api/api.js
+++ api/api.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 router.use('/', function(req, res, next){
 
-    res.set('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
 });
