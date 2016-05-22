85e6fc573936c9db853e0e29921d88cafa58403b
Linzee <d.gmiterko@gmail.com>
2016-05-22 13:12:36 +0200

    Disable elasticsearch (silly) logging

--- bin/data.js
+++ bin/data.js
@@ -2,7 +2,10 @@ var elasticsearch = require('elasticsearch');
 
 var client = new elasticsearch.Client({
   host: 'localhost:9200',
-  log: 'trace'
+  log : [{
+    type: 'stdio',
+    levels: ['error', 'warning']
+  }]
 });
 
 module.exports = client;
